378351c3d0f4e15233a44ce060babe8812029836
Dylan Cali <calid1984@gmail.com>
2015-10-10 08:40:41 -0500

    fix typo

--- site/src/site/pages/usage/default.groovy
+++ site/src/site/pages/usage/default.groovy
@@ -2,9 +2,9 @@ article {
     a(name: 'default'){}
     h2 { yield 'Default Version' }
     p {
-        yield 'Chose to make a given version the default:'
         pre { code '$ sdk default scala 2.11.6' }
         yield 'This will ensure that all subsequent shells will start with version 2.11.6 in use.'
+        yield 'Choose to make a given version the default:'
     }
 }
 br()
